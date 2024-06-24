cb11a60149807058da7427286f55f12ea551146f
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2024-06-24 00:31:04 -0400

    Update trend.py

--- trend.py
+++ trend.py
@@ -318,16 +318,16 @@ def create_plot(df, indicators):
             fig.add_trace(go.Scatter(x=df.index, y=df['tenkan_sen'], line=dict(color='blue', width=1), name='Tenkan-sen'))
             fig.add_trace(go.Scatter(x=df.index, y=df['kijun_sen'], line=dict(color='red', width=1), name='Kijun-sen'))
             fig.add_trace(go.Scatter(x=df.index, y=df['chikou_span'], line=dict(color='purple', width=1), name='Chikou Span'))
-            # Fill the cloud with different colors
-            for i in range(len(df)):
-                if df['senkou_span_a'][i] > df['senkou_span_b'][i]:
-                    color = 'rgba(0, 255, 0, 0.3)'  # Green color
-                else:
-                    color = 'rgba(255, 0, 0, 0.3)'  # Red color
-            fig.add_trace(go.Scatter(x=[df.index[i], df.index[i]],y=[df['senkou_span_a'][i], df['senkou_span_b'][i]],fill='tonexty',mode='lines',line=dict(width=0),fillcolor=color,showlegend=False))
-            # Plot Senkou Span A and B
-            fig.add_trace(go.Scatter(x=df.index, y=df['senkou_span_a'], line=dict(color='green', width=1), name='Senkou Span A'))
-            fig.add_trace(go.Scatter(x=df.index, y=df['senkou_span_b'], line=dict(color='red', width=1), name='Senkou Span B'))
+            # # Fill the cloud with different colors
+            # for i in range(len(df)):
+            #     if df['senkou_span_a'][i] > df['senkou_span_b'][i]:
+            #         color = 'rgba(0, 255, 0, 0.3)'  # Green color
+            #     else:
+            #         color = 'rgba(255, 0, 0, 0.3)'  # Red color
+            # fig.add_trace(go.Scatter(x=[df.index[i], df.index[i]],y=[df['senkou_span_a'][i], df['senkou_span_b'][i]],fill='tonexty',mode='lines',line=dict(width=0),fillcolor=color,showlegend=False))
+            # # Plot Senkou Span A and B
+            # fig.add_trace(go.Scatter(x=df.index, y=df['senkou_span_a'], line=dict(color='green', width=1), name='Senkou Span A'))
+            # fig.add_trace(go.Scatter(x=df.index, y=df['senkou_span_b'], line=dict(color='red', width=1), name='Senkou Span B'))
     # Make it pretty
     layout = go.Layout(
         plot_bgcolor='#efefef',
